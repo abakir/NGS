0ad9113debc9636c94d7c0c218dabf55c6dcbd19
saikamineni <ksaisree849@gmail.com>
2015-09-15 02:07:29 +0530

    growth rate

--- 9_revenue_growth_rate/get_product_growth_rate.py
+++ 9_revenue_growth_rate/get_product_growth_rate.py
@@ -16,15 +16,36 @@ for i in df.columns:
     if(p!=2):
         if(p!=len(df.columns)):
             col.append(i)
-            
+col.append('Average')
+                        
 df1=pd.DataFrame(columns=col)
 
 for i in range(0,max(df.index)+1):
     df1.loc[i,'Product']=df.loc[i,'Product']
-    for j in range(2,len(df.columns)-2):
+    for j in range(2,len(df.columns)-1):
         if(df.iloc[i,j-1]!=0):
             df1.iloc[i,j-1]=(df.iloc[i,j]-df.iloc[i,j-1])*100/df.iloc[i,j-1]
         else:
-            df1.iloc[i,j-1]=0
-            
+            k=j-1
+            flag=0
+            while(k!=1):
+                if(df.iloc[i,k]==0):
+                    k=k-1
+                else:
+                    df1.iloc[i,j-1]=(df.iloc[i,j]-df.iloc[i,k])*100/df.iloc[i,k]
+                    flag=1
+                    break
+            if(flag==0):
+                df1.iloc[i,j-1]=0
+
+for i in range(0,max(df.index)+1):
+    tot=0
+    for j in range(1,len(df.columns)-1):
+        if(df.iloc[i,j]!=0):
+            t=1
+            break
+    n=len(df.columns)-2-j
+    df1.loc[i,'Average']=(df.iloc[i,len(df.columns)-1]-df.iloc[i,j])*100/(n*df.iloc[i,j])
+    
+                            
 df1.to_csv("C:\Users\saisree849\Documents\GitHub\NGS_Project\\9_revenue_growth_rate\product_growth_rate.csv",index=False)
