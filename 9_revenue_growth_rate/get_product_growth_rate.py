ab4f5d27f86fba039494034ca4c7fcd94a468164
saikamineni <ksaisree849@gmail.com>
2015-10-15 22:06:41 +0530

    Dashboard

--- 9_revenue_growth_rate/get_product_growth_rate.py
+++ 9_revenue_growth_rate/get_product_growth_rate.py
@@ -1,5 +1,5 @@
 import pandas as pd
-location = 'C:\Users\saisree849\Documents\GitHub\NGS_Project\\9_revenue_growth_rate\\vend-total_revenue-for-product_variant-by-month (1).csv'
+location = 'C:\Users\saisree849\Documents\GitHub\NGS_Project\\9_revenue_growth_rate\\vend-total_revenue-for-product_variant-by-month.csv'
 df = pd.read_csv(location)
 
 df=df[0:max(df.index)-4]
@@ -9,35 +9,15 @@ n=[0]+n
 
 df=df[n]
 
-col=[]
-p=0
-for i in df.columns:
-    p=p+1
-    if(p!=2):
-        if(p!=len(df.columns)):
-            col.append(i)
-col.append('Average')
-col.append('Period')
+df = df.groupby('Product', axis=0, as_index=False).sum()
                         
-df1=pd.DataFrame(columns=col)
+df1=pd.DataFrame(columns=['Product', 'CAGR', 'Period'])
 
-for i in range(0,max(df.index)+1):
-    df1.loc[i,'Product']=df.loc[i,'Product']
-    for j in range(2,len(df.columns)-1):
-        if(df.iloc[i,j-1]!=0):
-            df1.iloc[i,j-1]=(df.iloc[i,j]-df.iloc[i,j-1])*100/df.iloc[i,j-1]
-        else:
-            k=j-1
-            flag=0
-            while(k!=1):
-                if(df.iloc[i,k]==0):
-                    k=k-1
-                else:
-                    df1.iloc[i,j-1]=(df.iloc[i,j]-df.iloc[i,k])*100/df.iloc[i,k]
-                    flag=1
-                    break
-            if(flag==0):
-                df1.iloc[i,j-1]=0
+def copyData(data):
+    return data
+    
+
+df1['Product'] = df.Product.apply(copyData)
 
 for i in range(0,max(df.index)+1):
     for j in range(1,len(df.columns)-1):
@@ -50,7 +30,7 @@ for i in range(0,max(df.index)+1):
     m=n**(-1)
     #df.iloc[i,len(df.columns)-1] is the total revnue from original table
     y=df.iloc[i,len(df.columns)-1]/df.iloc[i,j]
-    df1.loc[i,'Average']=(pow(y,m)-1)*100
+    df1.loc[i,'CMGR']=(pow(y,m)-1)*100
     df1.loc[i,'Period'] = n
     
                             
