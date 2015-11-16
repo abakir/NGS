cc5e9c229148c228db505208340904e1ff638838
saikamineni <ksaisree849@gmail.com>
2015-11-16 05:54:48 +0530

    dashboard

--- 12_dashboard tables/customers_table/get_order_frequency_customer.py
+++ 12_dashboard tables/customers_table/get_order_frequency_customer.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-df = pd.read_csv('C:\Users\saisree849\Documents\GitHub\NGS_Project\\12_dashboard tables\customers_table\orders_export.csv')
+df = pd.read_csv('C:\Users\saisree849\Documents\GitHub\NGS_Project\\12_dashboard tables\data\orders_export.csv')
 
 # take required fields
-df1=df[['Email', 'Created at' ]]
+df1=df[['Name','Email', 'Created at' ]]
 
 #let there be only one row for each order
 df1=df1.drop_duplicates()
@@ -15,9 +15,9 @@ df1['Created at'] = df1.apply(lambda x: x['Created at'][:10], axis = 1)
 df1=df1.drop_duplicates()
 df1= df1.reset_index().drop('index',1)
 
-df1.columns=['Email', 'Date']
+df1.columns=['Name','Email', 'Date']
 df1['Difference'] = 0
-df1['Count'] = 0
+df1['Total orders'] = 0
 
 df1['Date'] =pd.to_datetime(df1.Date)
 df1=df1.sort(['Email', 'Date'])
@@ -25,18 +25,18 @@ df1= df1.reset_index().drop('index',1)
 
 n = 1
 for i in range(0,max(df1.index)):
-    if(df1.iloc[i,0]==df1.iloc[i+1,0]):
+    if(df1.iloc[i,1]==df1.iloc[i+1,1]):
         n = n + 1
-        df1.iloc[i+1,2]=(df1.iloc[i+1,1]-df1.iloc[i,1]).days
+        df1.iloc[i+1,3]=(df1.iloc[i+1,2]-df1.iloc[i,2]).days
         if (i == max(df1.index) - 1):
-            df1.iloc[i + 1,3] = n
+            df1.iloc[i + 1,4] = n
     else:
-        df1.iloc[i,3] = n
+        df1.iloc[i,4] = n
         n = 1
 
 df1 = df1.groupby('Email', axis=0, as_index=False).sum()
-df1['Average'] = df1.apply(lambda x: x['Difference']/float(x['Count']), axis = 1)
-    
-df2 = df1[['Email' , 'Average',]]
+df1['Average days between orders'] = df1.apply(lambda x: x['Difference']/float(x['Total orders']), axis = 1)
+df2 = df1[['Email', 'Total orders', 'Average days between orders']]
+
             
 df2.to_csv("C:\Users\saisree849\Documents\GitHub\NGS_Project\\12_dashboard tables\customers_table\\frequency.csv",index=False)
--- 12_dashboard tables/customers_table/merge tables.py
+++ 12_dashboard tables/customers_table/merge tables.py
@@ -10,16 +10,30 @@ df10 = df1.merge(df3, on = ['Email'], how = 'inner')
 
 df6 = df10.merge(df5, on = ['Email'], how = 'inner')
 
-df2 = pd.read_csv("C:\Users\saisree849\Documents\GitHub\NGS_Project\\12_dashboard tables\customers_table\customers_export.csv")
+df6 = df6.merge(df, on = ['Email'], how = 'outer')
+
+df2 = pd.read_csv("C:\Users\saisree849\Documents\GitHub\NGS_Project\\12_dashboard tables\data\customers_export.csv")
 df2 = df6.merge(df2, on = ['Email'], how = 'inner')
 
-df2 = df2[['First Name', 'Last Name', 'Average', 'Email', 'Days from Last order', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', '0', '6', '12', '18']]
+df2 = df2[['First Name', 'Last Name', 'Total orders', 'Average days between orders', 'Email', 'Days from Last order', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', '0:00 - 2:00', '2:00 - 4:00', '4:00 - 6:00', '6:00 - 8:00', '8:00 - 10:00', '10:00 - 12:00', '12:00 - 14:00', '14:00 - 16:00', '16:00 - 18:00', '18:00 - 20:00', '20:00 - 22:00', '22:00 - 0:00']]
 
 for i in range(0, max(df2.index)+1):
     df2.loc[i, 'Name'] = df2.loc[i, 'First Name'] + " " + df2.loc[i, 'Last Name']
 
 df4 = df2.merge(df, on = ['Name', 'Email'], how = 'inner')
 
-df4 = df4[['Name', 'Revenue', 'Basket Value', 'Segment', 'Address','Phone', 'Average', 'Email', 'Days from Last order', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', '0', '6', '12', '18']]
+df4 = df4[['Name', 'Revenue', 'Basket Value', 'Segment', 'Address','Phone', 'Total orders', 'Average days between orders', 'Email', 'Days from Last order', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', '0:00 - 2:00', '2:00 - 4:00', '4:00 - 6:00', '6:00 - 8:00', '8:00 - 10:00', '10:00 - 12:00', '12:00 - 14:00', '14:00 - 16:00', '16:00 - 18:00', '18:00 - 20:00', '20:00 - 22:00', '22:00 - 0:00']]
 
+df = pd.read_csv('C:\Users\saisree849\Documents\GitHub\NGS_Project\\12_dashboard tables\data\orders_export.csv')
+df1 = df[['Lineitem quantity', 'Lineitem price']]
+df1['Revenue'] = df1.apply(lambda x: x['Lineitem quantity'] * x['Lineitem price'], axis=1)
+df1=df1[['Lineitem quantity', 'Revenue']]
+df = df[['Email']]
+df = df.drop_duplicates().reset_index().drop('index',1)
+
+df1 = df1.sum()
+df4['Average Revenue'] = df1['Revenue']/float(max(df.index)+1)
+df4['Average Basket Size'] = df1['Lineitem quantity']/float(max(df.index)+1)
+
+df4[['Total orders', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', '0:00 - 2:00', '2:00 - 4:00', '4:00 - 6:00', '6:00 - 8:00', '8:00 - 10:00', '10:00 - 12:00', '12:00 - 14:00', '14:00 - 16:00', '16:00 - 18:00', '18:00 - 20:00', '20:00 - 22:00', '22:00 - 0:00']] = df4[['Total orders', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', '0:00 - 2:00', '2:00 - 4:00', '4:00 - 6:00', '6:00 - 8:00', '8:00 - 10:00', '10:00 - 12:00', '12:00 - 14:00', '14:00 - 16:00', '16:00 - 18:00', '18:00 - 20:00', '20:00 - 22:00', '22:00 - 0:00']].astype(float)
 df4.to_csv("C:\Users\saisree849\Documents\GitHub\NGS_Project\\12_dashboard tables\customers_table\customers.csv",index = False)
