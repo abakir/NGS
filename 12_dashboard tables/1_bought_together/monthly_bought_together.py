af34bc136e7dad846d23fbf52c0848d64e248728
saikamineni <ksaisree849@gmail.com>
2016-11-29 23:38:57 +0530

    new changes

--- 12_dashboard tables/1_bought_together/monthly_bought_together.py
+++ 12_dashboard tables/1_bought_together/monthly_bought_together.py
@@ -16,90 +16,42 @@ def convertDate(data):
     matchobj = re.match(r'(.*)-(.*)-(.*).*',data)
     data = matchobj.group(1) + "-" + matchobj.group(2) + "-01"
     return data
-
-#count all the unique orders
-def getUnique(data):
-    return len(data) - len(set(data))
     
 #rows of type 21 and 12 are same, retain one instance of such rows
-def combineProds(date1, name1, name2, count):
+def combineProds(date1, name1, name2):
     if(name1 < name2):
-        return date1 + "," + name1 + "," + name2 + "," +count
+        return date1 + "," + name1 + "," + name2
     else:
-        return date1 + "," + name2 + "," + name1 + "," +count
-
-
+        return date1 + "," + name2 + "," + name1
+        
 df = df[['Name', 'Lineitem name', 'Created at']] #subset required columns
-df.columns = ['Name', 'Product', 'Date'] #rename columns
+df.columns = ['Order_id', 'Product', 'Date'] #rename columns
 
 #change product names to upper, remove duplicates, group the orders with same product, date
 df['Date'] = df.Date.apply(convertDate) 
 df['Product'] = df.Product.apply(lambda x: x.upper()) 
 df = df.drop_duplicates().reset_index().drop('index',1) 
-df = df.groupby(['Date', 'Product'], axis = 0, as_index=False)['Name'].sum() 
+df = df.groupby(['Date', 'Product'], axis = 0, as_index=False)['Order_id'].sum() 
 
-df['Name'] = df.Name.apply(lambda x: x.split("#")) #split the order no's by #
-df['Name'] = df.Name.apply(lambda x: x[1:]) #remove the extra comma
+df['Order_id'] = df.Order_id.apply(lambda x: x.split("#")) #split the order no's by #
+df['Order_id'] = df.Order_id.apply(lambda x: x[1:]) #remove the extra comma
 
-e = df['Date'].tolist() #create a list of dates
-#create a list of distinct dates
-d = []
-for i in e:
-    if i not in d:
-        d.append(i)
-        
-df1 = pd.DataFrame(columns = ['Date', 'Product1', 'Product2','Name', 'Count']) #create new dataframe
+df.columns = ['Date', 'Product1', 'Order1']
+df1 = pd.DataFrame(columns = ['Date', 'Product2', 'Order2'])
+df1['Date'] = df['Date']
+df1['Product2'] = df['Product1']
+df1['Order2'] = df['Order1']
 
-#creates date as index column
-df = df.set_index('Date') 
+#cartesian product
+df = pd.merge(df, df1,on='Date')[['Date', 'Product1', 'Product2', 'Order1', 'Order2']]
 
-pr1 = []
-pr2 = []
-dat = []
-for i in d: #traverse through all months
-    p1 = []
-    p2 = []
-    leng =  len(df.loc[i,'Product']) #count the products in each month
-    e = df.loc[i,'Product'].tolist() #make a list of all products in a month
-    for j in range(0, leng): #iterate through the length
-        p1 = p1 + e #add list of product 'leng' number of times
-    for k in e: 
-        for j in range(0, leng):
-            p2.append(k) #add each product 'leng' number of times
-            dat.append(i) #add each date 'leng' number of times
-    pr1 = pr1 + p1
-    pr2 = pr2 + p2
-df1['Date'] = pd.Series(dat) #update date column in new df
-df1['Product1'] = pd.Series(pr1) #update product1 column in new df
-df1['Product2'] = pd.Series(pr2) #update product2 column in new df
+df = df[df.Product1 != df.Product2]
+df['newcol'] = df.apply(lambda x: combineProds(x['Date'], x['Product1'], x['Product2']), axis=1)
+df = df.drop_duplicates('newcol').reset_index().drop('index', 1)
+df['Count'] = df.apply(lambda x: len(list(set(x['Order1']).intersection(x['Order2']))), axis=1)
 
-na = []
-for i in d:
-    #create a list of lists having orders for each product. 
-    e = df.loc[i,'Name'].tolist()
-    #add each list of orders for a product with all the list o orders of one product
-    for k in e:
-        for j in e:
-             p = k + j
-             na.append(p)
-df1['Name'] = pd.Series(na) #create a series for list 'na' and add to name column
+df = df[['Date', 'Product1', 'Product2', 'Count']]
+df = df[df.Count != 0]
+df = df.sort(['Count'], ascending=False).reset_index().drop('index', 1)
 
-df1 = df1[df1.Product1 != df1.Product2] #remove the rows whose product1 and product2 rows are same
-
-df1['Count'] = df1.Name.apply(getUnique)
-df1 = df1[df1.Count != 0] #remove rows with 0 count
-
-df1['newcol'] = df1.apply(lambda x: combineProds(str(x['Date']),x['Product1'], x['Product2'],str(x['Count'])), axis=1)
-df1 = df1.drop_duplicates('newcol').reset_index().drop('index',1)
-
-#get required products
-df1 = df1[['Date', 'Product1', 'Product2', 'Count']]
-
-#change the date format to datetime
-df1['Date'] = df1.Date.apply(lambda x: pd.to_datetime(datetime.strptime(x, '%Y-%m-%d')).date())
-
-#sort based on count in descending order
-df1 = df1.sort(['Count'], ascending = False)
-df1 = df1.reset_index().drop('index', 1)
-
-df1.to_csv(cfg['root']+cfg['output']+cfg['pair_by_month'])
+df.to_csv(cfg['root']+cfg['output']+cfg['pair_by_month'])
